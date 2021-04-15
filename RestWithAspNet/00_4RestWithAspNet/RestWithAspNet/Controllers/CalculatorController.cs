231e7b62220c0d1d27ff2692d7c561359d4029f9
Amilton dos Santos <60121840+amiltondssantos@users.noreply.github.com>
2021-04-15 20:08:24 -0300

    renomeando pastas
    renomeando pastas

---
+++ RestWithAspNet/00_4RestWithAspNet/RestWithAspNet/Controllers/CalculatorController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithAspNet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class calculatorController : ControllerBase
+    {
+
+        private readonly ILogger<calculatorController> _logger;
+
+        public calculatorController(ILogger<calculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult sum(string firstNumber, string secondNumber)
+        {
+                if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("substraction/{firstNumber}/{secondNumber}")]
+        public IActionResult substraction(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("divisao/{firstNumber}/{secondNumber}")]
+        public IActionResult divisao(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("multiplicacao/{firstNumber}/{secondNumber}")]
+        public IActionResult multiplicacao(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+
+        [HttpGet("media/{firstNumber}/{secondNumber}")]
+        public IActionResult media(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber))/2;
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("raizquadrada/{firstNumber}")]
+        public IActionResult raizquadrada(string firstNumber)
+        {
+            if (IsNumeric(firstNumber))
+            {
+                var raizquadrada = Math.Sqrt ((double) ConvertToDecimal(firstNumber));
+                return Ok(raizquadrada.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        private bool IsNumeric(string strNumber)
+          {
+            double number;
+            bool isNumber = double.TryParse(
+                strNumber, 
+                System.Globalization.NumberStyles.Any, 
+                System.Globalization.NumberFormatInfo.InvariantInfo, 
+                out number);
+            return isNumber;
+            
+          }
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+            
+        }
+
+       
+    }
+}
